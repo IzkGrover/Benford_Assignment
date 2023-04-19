5a4d8a20476da9da8c9dafbe4ebbf798aeebbc6d
Isa Khan <izkhan2006@gmail.com>
2023-04-19 13:50:17 -0400

    Update system.py

--- assignment/system.py
+++ assignment/system.py
@@ -1,4 +1,4 @@
-import matplotlib
+from matplotlib import pyplot as plt
 
 def printMenu():
     print('''
@@ -80,82 +80,42 @@ def analyzeFile(file):
 
     return percentages
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def generateGraph():
-    pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def generateGraph(percentages):
+
+    x_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y_values = percentages
+    y_value1 = "1 = " + str(y_values[0]) + "%"
+    y_value2 = "2 = " + str(y_values[1]) + "%"
+    y_value3 = "3 = " + str(y_values[2]) + "%"
+    y_value4 = "4 = " + str(y_values[3]) + "%"
+    y_value5 = "5 = " + str(y_values[4]) + "%"
+    y_value6 = "6 = " + str(y_values[5]) + "%"
+    y_value7 = "7 = " + str(y_values[6]) + "%"
+    y_value8 = "8 = " + str(y_values[7]) + "%"
+    y_value9 = "9 = " + str(y_values[8]) + "%"
+
+    plt.bar(x_values[0], y_values[0], color="red")
+    plt.bar(x_values[1], y_values[1], color="green")
+    plt.bar(x_values[2], y_values[2], color="blue")
+    plt.bar(x_values[3], y_values[3], color="orange")
+    plt.bar(x_values[4], y_values[4], color="yellow")
+    plt.bar(x_values[5], y_values[5], color="pink")
+    plt.bar(x_values[6], y_values[6], color="purple")
+    plt.bar(x_values[7], y_values[7], color="black")
+    plt.bar(x_values[8], y_values[8], color="brown")
+
+    plt.title("Benford's Law Distribution Leading Digit")
+    plt.legend([y_value1, y_value2, y_value3, y_value4, y_value5, y_value6, y_value7, y_value8, y_value9], loc=0)
+    plt.xlabel("Digit")
+    plt.ylabel("Percent")
+    plt.show()
+    plt.close()
+
+    table = "1" + "," + "2" + "," + "3" + "," + "4" + "," + "5", "," + "6" + "," + "7" + "," + "8" + "," + "9\n" + str(percentages[0]) + "," + str(percentages[1]) + "," + str(percentages[2]) + "," + str(percentages[3]) + "," + str(percentages[4]) + "," + str(percentages[5]) + "," + str(percentages[6]) + "," + str(percentages[7]) + "," + str(percentages[8])
+    result_fileName = "results.csv"
+    result_file = open(result_fileName, "w")
+    result_file.writelines(table)
+    result_file.close()
 
 userInput = ""
 fileLoad = "1"
@@ -174,7 +134,7 @@ while userInput != exitCondition:
         percentages = analyzeFile(file)
 
     elif userInput == graphGeneration: 
-        generateGraph()
+        generateGraph(percentages)
 
     else:
         print("Please type in a valid option (A number from 1-9)")
